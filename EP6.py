81a5114ac47fb00c03074282507f4f1ec8cabe14
devilnotalone <devilnotalone@gmail.com>
2022-04-11 14:42:04 +0700

    up2022_4_11

---
+++ EP6.py
@@ -0,0 +1,14 @@
+# type conversion
+x = 10
+y = 3.14
+z = "20"
+
+# บวกเลข
+result = x+y
+strZ = int(z)
+result2 = x+strZ
+print(result)
+print(result2)
+print(type(x))
+print(type(y))
+print(type(z))
